bef9fd83075cb3f4473a2aefd330b6e024163d3b
Monja Biendl <monja.biendl@gmail.com>
2024-06-02 11:48:59 +0200

    implemented logic for dynamic data

--- models/weatherStation.js
+++ models/weatherStation.js
@@ -1,32 +1,60 @@
-//TODO later db connection
-const getWeatherStations = async () => {
-//Static Data for now
-  return [
-    {
-      id: 1,
-      location: "Rettenbach",
-      weather_code: 800,
-      temperature: 20,
-      wind: 10,
-      pressure: 1000
-    },
-    {
-      id: 2,
-      location: "Regensburg",
-      weather_code: 800,
-      temperature: 18,
-      wind: 20,
-      pressure: 1000
-    },
-    {
-      id: 3,
-      location: "München",
-      weather_code: 800,
-      temperature: 22,
-      wind: 15,
-      pressure: 1000
-    }
-  ];
-};
+const db = require("./database.js");
 
-module.exports = getWeatherStations;
+const weatherStationModel = { 
+  async getAllStations() {
+    // function only used for debugging and until login and registration is implemented
+    //const query =  "SELECT * FROM weatherstation JOIN station_values ON weatherstation.id = station_values.weatherstation_id";
+    const query = `
+      SELECT * FROM weatherstation ws JOIN station_values sv ON ws.id = sv.weatherstation_id
+      JOIN (
+        SELECT weatherstation_id, MAX(data_time) AS max_time FROM station_values GROUP BY weatherstation_id
+      )
+      AS max_times ON sv.weatherstation_id = max_times.weatherstation_id AND sv.data_time = max_times.max_time;
+    `;
+    try{
+      const result = await db.getClient().query(query);
+      return result.rows;
+    } catch(error) {
+      console.error("Error fetching all stations:", error);
+      throw error;
+    }
+  },
+
+  async getStation(id) {
+    const query = 'SELECT * FROM weatherstation WHERE id = $1';
+    const values = [id];
+    try {
+      const result = await db.getClient().query(query, values);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error fetching the weather station with ID ${id}:", error);
+      throw error;
+    }
+  },
+
+  //TODO getUserStations(email)
+
+  async removeStation(id) {
+    const query = "DELETE FROM weatherstation WHERE id = $1";
+    const values = [id];
+    try{
+      await db.getClient().query(query, values);
+    } catch {
+      console.error("Unable to remove weather station with ID ${id}:", error);
+      throw error;
+    }
+  },
+
+  async addStation(station) {
+    const query = "INSERT INTO weatherstation (location, user_id) VALUES ($1, $2)";
+    const values = [station.location, station.userId];
+    try{
+      await db.getClient().query(query, values);
+    } catch {
+      console.error("Unable to add weather station:", error);
+      throw error;
+    }
+  }
+
+};
+module.exports = weatherStationModel;
