4dc3b73d3225f7442476b823e17ccdae004c5db7
Monja Biendl <monja.biendl@gmail.com>
2024-06-03 19:48:21 +0200

    added todos and error handling

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,22 +2,30 @@ const getStations = require("../models/weatherStation");
 
 const dashboard = {
     async index(request, response) {
-        const weatherStations = await getStations.getAllStations();
+        try{
+            const weatherStations = await getStations.getAllStationsWithLatestReading();
             const viewData = {
-            title: "Dashboard",
-            weatherStations: weatherStations
-        };
-        response.render("dashboard", viewData);
+                title: "Dashboard",
+                weatherStations: weatherStations
+            };
+            response.render("dashboard", viewData);  
+        } catch (error) {
+            console.error("Unable to fetch dashboard data: ", error);
+        }
+        
+        //TODO error handling
     },
     
     async addStation(request, response){
         //TODO
         response.redirect("/dashboard");
+        //TODO error handling
     },
 
     async deleteStation(request, response){
         //TODO
         response.redirect("/dashboard");
+        //TODO error handling
     }
 }
 
